785d4b8e247f1bb16e5bfc09cd0a9dab445f352b
Anas Ha <endoor-2020-020@outlook.com>
2020-04-03 10:54:40 +0200

    New Solution for Projects.

---
+++ Anas Hannoun/C#/Projects/Project Solution/Project Mini Bio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Mini_Bio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
